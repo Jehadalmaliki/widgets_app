2e8a8321a722045b0445d76a7ef4f42ddf67da2c
Jehadalmaliki <jehad@fastn.ai>
2025-01-06 00:21:53 +0300

    feat:install widgets package

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,57 @@
-import { useState } from "react";
-import Widget from "./widget";
-import "./App.css";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import FastnWidget from "@fastn-ai/widget-react";
+import qs from 'qs';
 
-function App() {
+const App = () => {
+  const [authToken, ] = useState('');
+  const [error, ] = useState('');
+
+  useEffect(() => {
+    const fetchAuthToken = async () => {
+      try {
+        const response = await axios.post('https://live.fastn.ai/auth/realms/fastn/protocol/openid-connect/token', 
+          qs.stringify({
+            grant_type: 'password',
+            username: 'automation@fastn.ai',
+            password: 'automation',
+            client_id: 'fastn-app',
+            scope: 'openid'
+          }), 
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            }
+          }
+        );
+        console.log('Access Token:', response.data.access_token);
+      } catch (error) {
+        console.error('Error fetching auth token:', error.response?.data || error.message);
+      }
+    };
+ 
+
+    fetchAuthToken();
+  }, []);
 
   return (
-    <>
-      <Widget />
-    </>
+    <div style={{ backgroundColor: 'black', height: '100vh' }}>
+      {error && (
+        <div style={{ color: 'red', textAlign: 'center' }}>
+          {error}
+        </div>
+      )}
+      <FastnWidget
+        style={{ backgroundColor: 'black' }}
+        projectId="a069807a-ef17-47cd-b4bc-ec29249049bd"
+        authToken={authToken}
+        tenantId="hello"
+        apiKey="0a7b75ee-55be-4190-b811-f070d3469599"
+        theme="light"
+        env="LIVE"
+      />
+    </div>
   );
-}
+};
 
 export default App;
